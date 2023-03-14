ada2c61385b623e5ea4fb27b01dcaac8185c493d
Devon Bernard <dwbcoding@gmail.com>
2023-03-14 14:47:04 -0500

    feat - created utility method for extracting tweet id from shared urls

--- server/app/utils/twitter.py
+++ server/app/utils/twitter.py
@@ -1,3 +1,5 @@
+import re
+from typing import Optional
 import tweepy
 from app.utils.config import get_config_value
 from app.typing.requests import MessageJson
@@ -18,6 +20,19 @@ def get_twitter_client():
     return client
 
 
+def get_tweet_id_from_string(text: str) -> (Optional[str], Optional[str]):
+    tweet_id = None
+    tweet_url = None
+    if text.startswith('https://twitter.com') and '/status/' in text:
+        message_parts = text.split('/status/', 1)
+        if message_parts and len(message_parts) == 2:
+            query_parts = message_parts[1].split('?', 1)
+            tweet_id = query_parts[0]
+            message_url_parts = text.split('?', 1)
+            tweet_url = message_url_parts[0]
+    return tweet_id, tweet_url
+
+
 def twitter_tweet(text: str):
     client = get_twitter_client()
     resp = client.create_tweet(text=text)
@@ -27,7 +42,7 @@ def twitter_tweet(text: str):
 def perform_twitter_task(message: MessageJson) -> TaskResponse:
     resp_data = {}
     if message['action'] == 'tweet':
-        resp = twitter_tweet(message['payload'])
+        resp = twitter_tweet(message['data'])
         if resp and len(resp.errors) == 0:
             tweet_id = resp.data['id']
             tweet_url = (
@@ -39,13 +54,18 @@ def perform_twitter_task(message: MessageJson) -> TaskResponse:
             )
             resp_data['tweetId'] = tweet_id
             resp_data['tweetUrl'] = tweet_url
-            return TaskResponse(True, [], resp_data)
-        else:
-            return TaskResponse(False, ['Unable to tweet'], {})
     else:
         return TaskResponse(
             False,
             [f'Unsupported twitter action: {message["action"]}'],
             {}
         )
-    return TaskResponse(False, ['Unable to perform twitter action'], {})
+
+    if resp and len(resp.errors) == 0:
+        return TaskResponse(True, [], resp_data)
+    else:
+        return TaskResponse(
+            False,
+            [f'Failed to perform twitter action: {message["action"]}'],
+            {}
+        )
