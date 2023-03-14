790540026d0eca5d13e208ad4b124ee2fd772bb6
Devon Bernard <dwbcoding@gmail.com>
2023-03-14 15:42:27 -0500

    mod - wrap react app with solana provider

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,9 +1,12 @@
 import 'dattt/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SolanaWalletProvider } from 'dattt/components/wallet'
 
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Component {...pageProps} />
+    <SolanaWalletProvider>
+      <Component {...pageProps} />
+    </SolanaWalletProvider>
   );
 }
