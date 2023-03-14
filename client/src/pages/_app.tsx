aaa9447822d7c711373ab6a1d6b0a7bb0c0b8df3
Devon Bernard <dwbcoding@gmail.com>
2023-03-14 17:12:26 -0500

    feat - created wireframe mvp of client dashboard

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,12 +1,36 @@
 import 'dattt/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SolanaWalletProvider } from 'dattt/components/wallet'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#512da8',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+    background: {
+      paper: '#27272a',
+      default: '#18181b',
+    },
+    // @ts-ignore
+    contrast: {
+      main: '#ccc'
+    }
+  }
+});
 
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SolanaWalletProvider>
-      <Component {...pageProps} />
-    </SolanaWalletProvider>
+    <ThemeProvider theme={theme}>
+      <SolanaWalletProvider>
+        <Component {...pageProps} />
+      </SolanaWalletProvider>
+    </ThemeProvider>
   );
 }
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,6 +1,15 @@
+import { useState } from 'react';
 import Head from 'next/head'
-import styles from 'dattt/styles/Home.module.css'
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
 
+import { WalletButton } from 'dattt/components/wallet';
 
 export default function Home() {
   return (
@@ -11,8 +20,109 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-      </main>
+      <AppBar position="static">
+        <Container maxWidth="lg">
+          <Toolbar>
+            <Typography variant="h4" sx={{ letterSpacing: -1, flexGrow: 1, fontWeight: 800 }}>
+              DATTT
+            </Typography>
+            <span >
+              <WalletButton />
+            </span>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Container maxWidth="sm">
+        <Grid container direction="column" spacing={2} sx={{mt:2}}>
+          <Grid item>
+            <Card sx={{p:2}}>
+            <Grid container spacing={0} direction={'column'}>
+                <Grid item sx={{pb:2}}>
+                  <TextField
+                    label="Tweet"
+                    variant="outlined"
+                    // @ts-ignore
+                    color="contrast"
+                    multiline
+                    minRows={2}
+                    maxRows={10}
+                    sx={{width: '100%'}}
+                  />
+                </Grid>
+                <Grid item sx={{display:'flex', alignItems: 'center', marginLeft: '1em'}}>
+                  <Button variant="contained" sx={{ml:'auto'}}>Tweet</Button>
+                </Grid>
+              </Grid>
+            </Card>
+          </Grid>
+          <Grid item>
+            <Card sx={{p:2}}>
+              <Grid container spacing={0} direction={'column'}>
+                <Grid item sx={{pb:2}}>
+                  <TextField
+                    label="Tweet Link"
+                    variant="outlined"
+                    // @ts-ignore
+                    color="contrast"
+                    sx={{width: '100%'}}
+                  />
+                </Grid>
+                <Grid item sx={{pb:2}}>
+                  <TextField
+                    label="Quote (Optional)"
+                    variant="outlined"
+                    // @ts-ignore
+                    color="contrast"
+                    multiline
+                    minRows={2}
+                    maxRows={10}
+                    sx={{width: '100%'}}
+                  />
+                </Grid>
+                <Grid item sx={{display:'flex', alignItems: 'center', marginLeft: '1em'}}>
+                  <Button variant="contained" sx={{ml:'auto'}}>Retweet</Button>
+                </Grid>
+              </Grid>
+            </Card>
+          </Grid>
+          <Grid item>
+            <Card sx={{p:2, mb:2}}>
+              <Grid container spacing={0}>
+                <Grid item sx={{flexGrow: 1}}>
+                  <TextField
+                    label="User"
+                    variant="outlined"
+                    // @ts-ignore
+                    color="contrast"
+                    sx={{width: '100%'}}
+                  />
+                </Grid>
+                <Grid item sx={{display:'flex', alignItems: 'center', marginLeft: '1em'}}>
+                  <Button variant="contained" sx={{ml:'auto'}}>Follow</Button>
+                </Grid>
+              </Grid>
+            </Card>
+          </Grid>
+          <Grid item>
+            <Card sx={{p:2, mb:2}}>
+              <Grid container spacing={0}>
+                <Grid item sx={{flexGrow: 1}}>
+                  <TextField
+                    label="User"
+                    variant="outlined"
+                    // @ts-ignore
+                    color="contrast"
+                    sx={{width: '100%'}}
+                  />
+                </Grid>
+                <Grid item sx={{display:'flex', alignItems: 'center', marginLeft: '1em'}}>
+                  <Button variant="contained" sx={{ml:'auto'}}>UnFollow</Button>
+                </Grid>
+              </Grid>
+            </Card>
+          </Grid>
+        </Grid>
+      </Container>
     </>
   )
 }
