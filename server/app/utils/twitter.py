eb8514d78382061d365db2954423509ccfebdff5
Devon Bernard <dwbcoding@gmail.com>
2023-03-13 23:49:59 -0500

    feat - created utility method for performing twitter tasks

--- server/app/utils/twitter.py
+++ server/app/utils/twitter.py
@@ -1,5 +1,7 @@
 import tweepy
 from app.utils.config import get_config_value
+from app.typing.requests import MessageJson
+from app.typing.tasks import TaskResponse
 
 
 def get_twitter_bot_handle() -> str:
@@ -20,3 +22,30 @@ def twitter_tweet(text: str):
     client = get_twitter_client()
     resp = client.create_tweet(text=text)
     return resp
+
+
+def perform_twitter_task(message: MessageJson) -> TaskResponse:
+    resp_data = {}
+    if message['action'] == 'tweet':
+        resp = twitter_tweet(message['payload'])
+        if resp and len(resp.errors) == 0:
+            tweet_id = resp.data['id']
+            tweet_url = (
+                'https://twitter.com/{handle}/status/{tweet_id}'
+                .format(
+                    tweet_id=tweet_id,
+                    handle=get_twitter_bot_handle()
+                )
+            )
+            resp_data['tweetId'] = tweet_id
+            resp_data['tweetUrl'] = tweet_url
+            return TaskResponse(True, [], resp_data)
+        else:
+            return TaskResponse(False, ['Unable to tweet'], {})
+    else:
+        return TaskResponse(
+            False,
+            [f'Unsupported twitter action: {message["action"]}'],
+            {}
+        )
+    return TaskResponse(False, ['Unable to perform twitter action'], {})
